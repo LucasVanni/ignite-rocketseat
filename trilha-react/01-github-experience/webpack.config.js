7c30e4e62cac5a6923a868271a61c4417df255d9
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-13 12:29:51 -0300

    feat: sorting out dev and production envoriment

--- trilha-react/01-github-experience/webpack.config.js
+++ trilha-react/01-github-experience/webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
