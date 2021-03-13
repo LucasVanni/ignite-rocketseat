d69a376cc81399c1c704776782f41327055dcc6c
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-13 11:36:57 -0300

    feat: html webpack plugin configured

--- trilha-react/01-github-experience/src/index.jsx
+++ trilha-react/01-github-experience/src/index.jsx
@@ -1,2 +1,4 @@
-import React from "react";
-import {App} from './App';
+import { render } from "react-dom";
+import { App } from "./App";
+
+render(<App />, document.getElementById("root"));
--- trilha-react/01-github-experience/webpack.config.js
+++ trilha-react/01-github-experience/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -9,13 +11,18 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [
-        {
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            use: 'babel-loader',
-        }
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
     ],
   },
 };
