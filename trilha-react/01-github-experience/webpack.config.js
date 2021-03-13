28a3a28375aafcc13893aac405acc9ef326969d6
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-13 12:52:02 -0300

    feat: loaders for css configured

--- trilha-react/01-github-experience/webpack.config.js
+++ trilha-react/01-github-experience/webpack.config.js
@@ -29,6 +29,12 @@ module.exports = {
         exclude: /node_modules/,
         use: "babel-loader",
       },
+
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
 };
