f52c47cf85ac993631e3a8d8f6952f793032961b
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-13 20:25:33 -0300

    feat: fetch repositories from github api and setRepositories state

--- trilha-react/01-github-experience/src/components/RepositoryList.jsx
+++ trilha-react/01-github-experience/src/components/RepositoryList.jsx
@@ -1,6 +1,11 @@
+import { useEffect, useState } from "react";
 import RepositoryItem from "./RepositoryItem";
 
 import "../styles/repositories.scss";
+
+// https://api.github.com/orgs/rocketseat/repos
+// https://api.github.com/users/lucasvanni
+// https://api.github.com/users/LucasVanni/repos
 
 const repository = {
   name: "teste",
@@ -9,6 +14,17 @@ const repository = {
 };
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    // Faz a chamada para a api do github
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      // Transforma a resposta em json
+      .then((response) => response.json())
+      // Disponibiliza os dados para serem usados
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
