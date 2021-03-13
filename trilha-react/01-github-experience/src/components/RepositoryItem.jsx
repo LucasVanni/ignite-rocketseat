78c4d03e2ea2776c323d47ca0fc5e1583ed93dec
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-13 20:36:06 -0300

    feat: listing repositories

--- trilha-react/01-github-experience/src/components/RepositoryItem.jsx
+++ trilha-react/01-github-experience/src/components/RepositoryItem.jsx
@@ -4,14 +4,9 @@ const RepositoryItem = (props) => {
       <strong>{props.repository?.name ?? "Default name"}</strong>
       <p>{props.repository?.description ?? "Default description"}</p>
 
-      <a href={props.repository?.link}>Acessar repositório</a>
+      <a href={props.repository?.html_url}>Acessar repositório</a>
     </li>
   );
 };
 
 export default RepositoryItem;
-
-
-
-
-
--- trilha-react/01-github-experience/src/components/RepositoryList.jsx
+++ trilha-react/01-github-experience/src/components/RepositoryList.jsx
@@ -7,21 +7,12 @@ import "../styles/repositories.scss";
 // https://api.github.com/users/lucasvanni
 // https://api.github.com/users/LucasVanni/repos
 
-const repository = {
-  name: "teste",
-  description: "Bem vindo ao teste",
-  link: "https://github.com/",
-};
-
 const RepositoryList = () => {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    // Faz a chamada para a api do github
     fetch("https://api.github.com/orgs/rocketseat/repos")
-      // Transforma a resposta em json
       .then((response) => response.json())
-      // Disponibiliza os dados para serem usados
       .then((data) => setRepositories(data));
   }, []);
 
@@ -30,8 +21,9 @@ const RepositoryList = () => {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
