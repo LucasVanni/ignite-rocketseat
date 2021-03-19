cdf885cc5e521162bae75d12eb86beb316e356a1
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-19 20:19:10 -0300

    feat: criando tipagem para as categorias

--- trilha-node/iniciando-api/criando-api-nodejs/src/routes/categories.routes.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/routes/categories.routes.ts
@@ -1,18 +1,20 @@
 import { Router, Request, Response } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (req: Request, res: Response) => {
     const { name, description } = req.body;
 
-    const category = {
-        id: uuidV4(),
+    const category = new Category();
+
+    Object.assign(category, {
         name,
         description,
-    };
+        created_at: new Date(Date.now()),
+    });
 
     categories.push(category);
 
