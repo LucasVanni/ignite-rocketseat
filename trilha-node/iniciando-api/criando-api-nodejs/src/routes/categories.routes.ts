65db1cd26948b0a0d9ba2501f780b6d7ba983f44
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-19 09:16:18 -0300

    feat: inserindo uuid

--- trilha-node/iniciando-api/criando-api-nodejs/src/routes/categories.routes.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router, Request, Response } from 'express';
+import { v4 as uuidV4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (req: Request, res: Response) => {
+categoriesRoutes.post('/', (req: Request, res: Response) => {
     const { name, description } = req.body;
 
-    categories.push({
+    const category = {
+        id: uuidV4(),
         name,
         description,
-    });
+    };
+
+    categories.push(category);
 
     return res.status(201).json(categories);
 });
--- trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running 🚀'));
