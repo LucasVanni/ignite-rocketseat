90fafe2ac916ee338baef0af9123ff8224edd2da
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-19 07:00:58 -0300

    feat: debugando a aplicação

--- trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (_req, res) => {
     res.send('Welcome my friends');
+});
+
+app.post('/courses', (req, res) => {
+    const { name } = req.body;
+
+    return res.status(201).json({ name });
 });
 
 app.listen(3333, () => console.log('Server is running 🚀'));
