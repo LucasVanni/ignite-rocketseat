324c5fc36b155db20fec95601bed209d60fe41b4
Lucas Vanni <lucas.vanni@hotmail.com>
2021-03-20 18:47:56 -0300

    feat: LSP implementados

--- trilha-node/iniciando-api/solid/src/services/CreateCategoryService.ts
+++ trilha-node/iniciando-api/solid/src/services/CreateCategoryService.ts
@@ -1,4 +1,5 @@
 import CategoriesRepository from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
@@ -6,7 +7,7 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-    constructor(private categoriesRepository: CategoriesRepository) {}
+    constructor(private categoriesRepository: ICategoriesRepository) {}
 
     execute({ name, description }: IRequest): void {
         const category = this.categoriesRepository.findByName(name);
