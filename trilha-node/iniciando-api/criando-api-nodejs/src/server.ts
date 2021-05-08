53a82b0f3505c5346082ce7a9845d00caa9bca27
Lucas Vanni <lucas.vanni@hotmail.com>
2021-05-08 16:21:54 -0300

    feat: add typeorm and fix: docker compose

--- trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/server.ts
@@ -1,15 +1,5 @@
-import express from 'express';
-import swaggerUI from 'swagger-ui-express';
-import { router } from './routes';
+import app from './app';
 
-import swaggerFile from './swagger.json';
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
-app.use(router);
+import './database';
 
 app.listen(3333, () => console.log('Server is running 🚀'));
