85709688878fc17159cfbcce202a7fd66961823e
Lucas Vanni <lucas.vanni@hotmail.com>
2021-05-23 14:10:13 -0300

    feat: refatorando model de categorias

---
+++ trilha-node/iniciando-api/criando-api-nodejs/src/modules/cars/entites/Category.ts
@@ -0,0 +1,36 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+/*
+    Decorator para informar ao typeorm 
+    a relação desse model que agora é uma entity,
+    com a tabela categories que foi criada pela 
+    migration
+*/
+@Entity('Categories')
+class Category {
+    // Coluna primária, que será o id da migration.
+    @PrimaryColumn()
+    id?: string;
+
+    // Referencia a coluna name com o name da migration.
+    @Column()
+    name: string;
+
+    // Referencia a coluna description com o description da migration.
+    @Column()
+    description: string;
+
+    // Referencia a coluna created_at com o created_at da migration.
+    // Além de referenciar é um decorator específico para created_at.
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { Category };
