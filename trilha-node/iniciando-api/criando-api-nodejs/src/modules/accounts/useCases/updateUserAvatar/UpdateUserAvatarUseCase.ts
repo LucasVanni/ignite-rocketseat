c01e0f4e2e6de20e855abee214e1137513c063d2
Lucas Vanni <lucas.vanni@hotmail.com>
2021-08-26 21:03:07 -0300

    feat: remove avatar duplicated on application

--- trilha-node/iniciando-api/criando-api-nodejs/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ trilha-node/iniciando-api/criando-api-nodejs/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
+import { deleteFile } from '../../../../utils/file';
+
 interface IRequest {
     user_id: string;
     avatar_file: string;
@@ -16,6 +18,7 @@ class UpdateUserAvatarUseCase {
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
 
+        await deleteFile(`./tmp/avatar/${user.avatar}`);
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
